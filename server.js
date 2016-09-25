d0782b3e0adc110bd0e342f6c2929f9a9506eb74
zuhalmazood <zuhalmazood@gmail.com>
2016-09-25 17:45:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/mypage', function (req, res ) { 
+    res.send( 'Myfile is requested');
+});
 app.get('/man.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
